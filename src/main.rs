f8eb185057ced5d7e389e2ec8027223bcef5af0b
Josue Morales <molinajosue92@hotmail.com>
2023-09-09 16:59:19 -0400

    Add Files

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use animeboys_bot::bot::Bot;
+use serenity::prelude::*;
+
+/// To Cross compile for linux
+/// cross build --release --target armv7-unknown-linux-gnueabihf
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .with_max_level(tracing::Level::DEBUG)
+        .compact()
+        .init();
+    let token = std::env::var("DISCORD_TOKEN").expect("DISORD_TOKEN is required");
+    let instance_id = std::env::var("INSTANCE_ID").expect("INSTANCE_ID is required");
+
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Bot::new(instance_id).await)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Error starting client: {:?}", why);
+    }
+}
