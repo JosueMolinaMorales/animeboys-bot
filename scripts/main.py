10d551df63a1abd00a591ce1dfa76e4aa8f5b581
Josue Morales <molinajosue92@hotmail.com>
2023-10-03 21:53:15 -0400

    add in chatgpt

--- scripts/main.py
+++ scripts/main.py
@@ -16,15 +16,40 @@ def find_unique_keys(json_obj, parent_key='', separator='.'):
             keys.extend(find_unique_keys(item, new_key, separator))
     return keys
 
+def find_all_weapon_ids(builds):
+    weapon_ids = []
+    for build in builds:
+        weapon_ids.append(build['weaponId'])
 
-# Example JSON object
-with open("output.json", 'r') as f:
-    json_obj = json.load(f)
-    # Find and print unique keys
+    # Remove all duplicates
+    weapon_ids = list(set(weapon_ids))
+    return weapon_ids
 
-    keys = find_unique_keys(json_obj['builds'])
-    keys.sort()
-    unique_keys = set(keys)
-    for key in unique_keys:
-        if str(key).count('.') == 0:
-            print(key)
+def main():
+    # Display the menu
+    print("=============================================")
+    print("1. Find all unique keys")
+    print("2. Find all weapon IDs")
+    print("=============================================")
+    print("Enter your choice: ")
+    choice = input()
+
+    # Load json object
+    json_obj = json.load(open("output.json", 'r'))
+    if choice == '1':
+        keys = find_unique_keys(json_obj['builds'])
+        keys.sort()
+        unique_keys = set(keys)
+        for key in unique_keys:
+            if str(key).count('.') == 0:
+                print(key)
+    elif choice == '2':
+        weapon_ids = find_all_weapon_ids(json_obj['builds'])
+        weapon_ids.sort()
+        for weapon_id in weapon_ids:
+            print(weapon_id)
+
+
+
+if __name__ == '__main__':
+    main()
